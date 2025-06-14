15b67c40a197fcc7f3cf78241152f83e9fcb5ccc
gloryfoster <gf392@drexel.edu>
2025-06-13 22:02:49 -0400

    Added page movement script

--- deploy/script.js
+++ deploy/script.js
@@ -5,3 +5,20 @@ document.addEventListener('DOMContentLoaded', function() {
         main.classList.toggle('active');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.associations-img');
+
+    function checkImages() {
+        const windowTrigger = window.innerHeight * 0.9;
+        images.forEach(img => {
+            const imgTop = img.getBoundingClientRect().top;
+            if (imgTop < windowTrigger) {
+                img.classList.add('visible');
+            }
+        });
+    };
+
+    window.addEventListener('scroll', checkImages);
+    checkImages();
+});
